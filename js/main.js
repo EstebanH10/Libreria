1e83b0e9cad3a2869670e5ae99bed3e69b025d08
Esteban <herranesteban9@gmail.com>
2023-07-28 23:12:19 -0500

    Mi primer commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function(){
+    document.querySelector('.aside').classList.toggle('active');
+});
+
+const btnToggle_main = document.querySelector('.toggle-btn');
+
+btnToggle_main.addEventListener('click', function(){
+    document.querySelector('.main').classList.toggle('mainActive');
+});
